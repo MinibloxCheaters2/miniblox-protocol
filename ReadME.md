3970c1039566763c60fb222e19442233a5848628
bab <bab@noreply.codeberg.org>
2026-06-12 16:47:42 -0500

    chore: init

---
+++ ReadME.md
@@ -0,0 +1,3 @@
+# @miniblox/protocol
+
+Just contains generated protobuf definitions for `@miniblox/bot` and `@miniblox/server` (TODO: actually make these).
---
+++ tsdown.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, type UserConfig } from "tsdown";
+
+export default defineConfig({
+  entry: "src/index.ts",
+  dts: true,
+  platform: "node", // NO ONE wants ts on a browser lol
+  target: "esnext",
+  cjsDefault: false, // no. CommonJS is trash, grow up.
+  hash: false,
+}) satisfies UserConfig as UserConfig;
